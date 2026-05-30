474565c5a2bf646b72bd51138460961b3a7aedb8
aleksandra.godes <san_shine7@list.ru>
2026-05-31 03:53:58 +0500

    1

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-const img = document.getElementById('img');
-const predictionsElem = document.getElementById('predictions');
-// Load the model.
-mobilenet.load().then(model =>
-{
-    // Classify the image.
-    model.classify(img).then(predictions =>
-    {
-        var serializedPredictions = JSON.stringify(predictions);
-        // View in browser in string version
-        predictionsElem.innerHTML = serializedPredictions;
-        // View in console in object version
-        console.log(predictions);
-    });
+const img = document.getElementById('input-image');
+
+// Initialize the model
+cocoSsd.load().then(model => {
+  // Detect objects within the HTML image element
+  model.detect(img).then(predictions => {
+    console.log('Predictions: ', predictions);
+  });
 });
